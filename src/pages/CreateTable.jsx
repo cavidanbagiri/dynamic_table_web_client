8f85b32479006dfe8b84afbf0d67a844feaf9b75
CavidanBagirliCamal <cbtechauto@gmail.com>
2025-01-08 15:07:05 +0300

    change font and design for create table page

--- src/pages/CreateTable.jsx
+++ src/pages/CreateTable.jsx
@@ -21,7 +21,7 @@ function CreateTable() {
 
     const [file, setFile] = useState(null);
     const [table_name, setTableName] = useState(null);
-    const [table_status, setTableStatus] = useState(null);
+    const [table_status, setTableStatus] = useState('public');
     const [table_description, setTableDescription] = useState(null);
 
     const handleFileChange = (e) => {
@@ -43,7 +43,8 @@ function CreateTable() {
             formData.append('table_status', table_status);
             formData.append('table_description', table_description);
             formData.append('table_name', replace_table_name);
-            dispatch(TableService.createTable(formData));
+            console.log('object is ', formData);
+            // dispatch(TableService.createTable(formData));
         }
     }
 
@@ -57,7 +58,7 @@ function CreateTable() {
     }, [show_message])
 
     return (
-        <div className='flex flex-col items-start bg-red-500'>
+        <div className='flex flex-col items-start text-gray-500'>
 
             {show_message === 1 ? (
                 <MessageBox message={created_table.message} color={'bg-green-500'} />
@@ -65,22 +66,36 @@ function CreateTable() {
                 <MessageBox message={created_table.message} color={'bg-red-500'} />
             ) : null}
 
-            <span>Create Table From excel(.xlsx) or from csv(.csv)</span>
+            <span className='text-3xl text-black font-medium my-5'>Create Table</span>
 
+            <label htmlFor="file" className='text-sm text-gray-400'>Choose .xlsx or csv file</label>
             <input type="file" onChange={handleFileChange} />
 
-            <input type="text" onChange={(e) => setTableName(e.target.value)} placeholder='Table Name' />
+            <input type="text" onChange={(e) => setTableName(e.target.value)} placeholder='Table Name'
+                className='mt-4 mb-2 border p-2 rounded-md outline-gray-400 w-64'
+            />
 
-            <input type="text" onChange={(e) => setTableStatus(e.target.value)} placeholder='Table Status' />
+            {/* <input type="text" onChange={(e) => setTableStatus(e.target.value)} placeholder='Table Status' /> */}
+            <select name="table_status" onChange={(e) => setTableStatus(e.target.value)} id="" className='my-2 border p-2 rounded-md outline-gray-400 w-64'>
+                <option value="public">Public</option>
+                <option value="private">Private</option>
+            </select>
 
-            <input type="text" onChange={(e) => setTableDescription(e.target.value)} placeholder='Table Description' />
+
+            <textarea name="" id="" cols="30" rows="10" 
+            onChange={(e) => setTableDescription(e.target.value)}
+            placeholder='Table Description' 
+            className='my-2 border p-2 rounded-md outline-gray-400 w-64'>
+
+            </textarea>
+
             {
                 created_table.pending ?
                     <ButtonLoader />
                     :
                     <button
                         onClick={handleUploadClick}
-                        className='flex items-center my-2 border p-2 bg-green-500 text-white rounded-md text-sm font-medium hover:bg-green-300 hover:text-white duration-200'>
+                        className='flex items-center justify-center my-2 border p-2 w-64 bg-green-500 text-white rounded-md text-md font-medium hover:bg-green-300 hover:text-white duration-200'>
                         Create
                     </button>
             }
